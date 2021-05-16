f5ecd88d37c2fc1c1d8bae981bd27455e60771be
Mizuho <mizuho128@gmail.com>
2021-05-16 20:14:06 +0900

    glob flac for NT/POSIX, condition for empty np.where and exception handling

--- fix_piano_in_162.py
+++ fix_piano_in_162.py
@@ -3,24 +3,37 @@ import numpy as np
 import argparse
 import glob
 import os
+import traceback
 from tqdm import tqdm
 
 def func(args):
-    files = glob.glob(os.path.join(args.input_dir, '**\*.flac'))
+    flac_pattern = '**\*.flac' if os.name == "nt" else '**/*.flac'
+    files = glob.glob(os.path.join(args.input_dir, flac_pattern), recursive=True)
     print(f'{len(files)} flac files are found.')
+
+    exceps = {}
     for file in files:
         try:
             data, freq = sf.read(file)
-        except:
+            body_idxs = np.where(data > 0.001)
+
+            if len(body_idxs[0]) == 0:
+                print(f'skip {file}')
+                continue
+
+            pos = body_idxs[0][0]
+            if pos > 0:
+                zcpos = np.where(data[0:pos] <= 0.0)[0]
+                if len(zcpos):
+                    zcpos = zcpos[-1] + 1
+                    data = data[zcpos:]
+                    sf.write(file, data, freq, format='FLAC', subtype='PCM_24')
+                    print(f'{zcpos}\t{file[-70:]}')
+        except Exception as e:
+            exceps[file] = traceback.format_exc()
+            print(f'file: {file}\n{exceps[file]}')
             continue
-        pos = np.where(data > 0.001)[0][0]
-        if pos > 0:
-            zcpos = np.where(data[0:pos] <= 0.0)[0]
-            if len(zcpos):
-                zcpos = zcpos[-1] + 1
-                data = data[zcpos:]
-                sf.write(file, data, freq, format='FLAC', subtype='PCM_24')
-                print(f'{zcpos}\t{file[-70:]}')
+    return exceps
 
 def main():
     parser = argparse.ArgumentParser(
@@ -34,7 +47,8 @@ def main():
     parser.set_defaults(func=func)
 
     args = parser.parse_args()
-    args.func(args)
+    exceps = args.func(args)
+    exit(len(exceps))
 
 if __name__ == '__main__':
     main()
